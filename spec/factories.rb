4f1224f5f2240b317bca071ec111d8c83e594a98
bfetler <bfetler@gmail.com>
2012-04-09 22:38:09 -0700

    set factory method user email

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :user do
 #   sequence(:name) { |n| "Person #{n}" }
-    sequence(:email) { |n| "person_#{n}@example.com"}
+#   sequence(:email) { |n| "person_#{n}@example.com"}
+    email "my.email@foo.com"
     password "foobar"
     password_confirmation "foobar"
 
